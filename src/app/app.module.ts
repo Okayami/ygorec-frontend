f5d110335c1fa27a5889c271731e4de7697567c9
Okayami <46897189+Okayami@users.noreply.github.com>
2024-02-16 13:36:47 +0100

    remove useless component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,23 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { AboutUsComponent } from './about-us/about-us.component';
-import { HowItWorksComponent } from './how-it-works/how-it-works.component';
 import { RouterModule, Routes } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { NavbarComponent } from './navbar/navbar.component';
+import { HomePageComponent } from './home-page/home-page.component';
 
 const routes:Routes = [
-  {path:'about-us', component:AboutUsComponent},
-  {path:'how-it-works', component:HowItWorksComponent}
+  {path:'', component:HomePageComponent}
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
-    AboutUsComponent,
-    HowItWorksComponent,
-    NavbarComponent
+    NavbarComponent,
+    HomePageComponent
   ],
   exports:[RouterModule],
   imports: [
