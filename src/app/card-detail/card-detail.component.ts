4f3da353a2cf6719386212cfaba2ff91463c1e31
Okayami <46897189+Okayami@users.noreply.github.com>
2024-03-19 17:31:30 +0100

    wsh

--- src/app/card-detail/card-detail.component.ts
+++ src/app/card-detail/card-detail.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
-import { Observable } from 'rxjs';
+import { ActivatedRoute, NavigationEnd, NavigationStart, Router, RoutesRecognized } from '@angular/router';
+import { filter } from 'rxjs/internal/operators/filter';
 import { CardList } from 'src/shared/model/card-list';
 import { Card } from 'src/shared/model/card.model';
 import { CardService } from 'src/shared/service/card.service';
@@ -16,6 +16,8 @@ export class CardDetailComponent implements OnInit {
   public cardId: number = 0;
   public playedCards: CardList[] = [];
   public totalDeck: number = 0;
+  public limit: number = 30;
+  public offset: number = 0;
   public selectedCard: Card = {
     ID: 0,
     Name: "",
@@ -31,8 +33,33 @@ export class CardDetailComponent implements OnInit {
     Level: ""
   }
 
-  constructor(private readonly cardService: CardService, private router: Router) { }
+  constructor(private readonly cardService: CardService, private router: Router) {
+    router.events.subscribe((val) => {
+      if (val instanceof NavigationEnd) {
+        this.selectedCard = {
+          ID: 0,
+          Name: "",
+          Desc: "",
+          Attribute: "",
+          Race: "",
+          Atk: "",
+          Def: "",
+          Types: [],
+          Image: "",
+          Symbol: "",
+          Limitation: 0,
+          Level: ""
+        }
+        this.cardId = 0;
+        this.playedCards = [];
+        this.ngOnInit();
+      }
+    });
+  }
+
   ngOnInit(): void {
+    console.log("start");
+    console.log(window.location.href);
     this.currentURL = window.location.href;
     if (this.cardId == 0) {
       this.cardId = Number(this.currentURL.substring(this.currentURL.lastIndexOf("/") + 1, this.currentURL.length));
@@ -46,7 +73,7 @@ export class CardDetailComponent implements OnInit {
         this.selectedCard.Symbol = "assets/Symbol/" + this.selectedCard.Attribute + ".png";
       }
     });
-    this.cardService.getPlayedCards(this.cardId).subscribe((reponse) => {
+    this.cardService.getPlayedCards(this.limit, this.offset, this.cardId).subscribe((reponse) => {
       this.playedCards = reponse.data.List;
       this.totalDeck = Number(reponse.data.DeckAmount);
     });
@@ -55,21 +82,15 @@ export class CardDetailComponent implements OnInit {
   goToCard(item: CardList): void {
     this.cardId = item.ID;
     this.router.navigate(['/card-detail/', item.ID]);
-    this.selectedCard = {
-      ID: 0,
-      Name: "",
-      Desc: "",
-      Attribute: "",
-      Race: "",
-      Atk: "",
-      Def: "",
-      Types: [],
-      Image: "",
-      Symbol: "",
-      Limitation: 0,
-      Level: ""
-    }
-    this.playedCards = [];
-    this.ngOnInit();
+  }
+
+  loadMore(): void {
+    this.offset = this.offset + 30;
+    this.cardService.getPlayedCards(this.limit, this.offset, this.cardId).subscribe((reponse)=> {
+      reponse.data.List.forEach((currentValue: Card, index: number) => {
+        this.totalDeck = Number(reponse.data.DeckAmount);
+        this.playedCards.push(reponse.data.List[index]);
+      });
+    })
   }
 }
